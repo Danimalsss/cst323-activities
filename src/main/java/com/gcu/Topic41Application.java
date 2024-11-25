b753f08e1f143d8554e9750f9387b8a607672b8f
Daniel Thompson <dthompson132@my.gcu.edu>
2024-11-24 23:37:23 -0800

    Signed-off-by: Daniel Thompson <dthompson132@my.gcu.edu>

--- src/main/java/com/gcu/Topic41Application.java
+++ src/main/java/com/gcu/Topic41Application.java
@@ -3,7 +3,9 @@ package com.gcu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @ComponentScan({ "com.gcu" })
 @SpringBootApplication
 public class Topic41Application {
